43dd3bf617c66cf26b7080a7880e696d5034f81d
OhShame <lilianshame@gmail.com>
2024-05-27 18:03:28 +0300

    Выполнение задания 1

---
+++ src/main/java/ru/sberbank/jd/lesson01/GreetingImp.java
@@ -0,0 +1,121 @@
+/**
+ * пакет с задачами для урока 1.
+ */
+
+package ru.sberbank.jd.lesson01;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Класс по заданию 1.
+ */
+public class GreetingImp implements Greeting {
+
+    /**
+     * Объявляем переменные.
+     */
+
+    private final String firstName;
+    private final  String lastName;
+    private final int birthYear;
+    private final Set<String> hobbies;
+    private final String repoUrl;
+    private final String phone;
+    private final Set<String> courseExpectations;
+
+    /**
+     * Конструктор класса с параметрами.
+     */
+
+    public GreetingImp(String firstName, String lastName, int birthYear,
+                       Collection<String> hobbies, String repoUrl,
+                       String phone, Collection<String> courseExpectations) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.hobbies = Collections.unmodifiableSet(new HashSet<>(hobbies));
+        this.repoUrl = repoUrl;
+        this.phone = phone;
+        this.courseExpectations = Collections.unmodifiableSet(new HashSet<>(courseExpectations));
+    }
+
+    /**
+     *Геттер для имени (перегружен от интерфейса).
+     */
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+    /**
+     *Геттер для фамилии (перегружен от интерфейса).
+     */
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+    /**
+     *Геттер длядаты рождения (перегружен от интерфейса).
+     */
+
+    @Override
+    public int getBirthYear() {
+        return birthYear;
+    }
+    /**
+     *Геттер для коллекции с хобби (перегружен от интерфейса).
+     */
+
+    @Override
+    public Collection<String> getHobbies() {
+        return hobbies;
+    }
+    /**
+     *Геттер для репозитория (перегружен от интерфейса).
+     */
+
+    @Override
+    public String getRepoUrl() {
+        return repoUrl;
+    }
+
+    /**
+     *Геттер для телефона (перегружен от интерфейса).
+     */
+
+    @Override
+    public String getPhone() {
+        return phone;
+    }
+    /**
+     *Геттер для ожиданий от курса(перегружен от интерфейса).
+     */
+
+    @Override
+    public Collection<String> getCourseExpectations() {
+        return courseExpectations;
+    }
+    /**
+     *псвм метод для инициализации объекта (себя х).
+     */
+
+    public static void main(String[] args) {
+        String firstName = "Анна";
+        String lastName = "Гасанова";
+        int birthYear = 1995;
+        Collection<String> hobbies = new ArrayList<>();
+        hobbies.add("видеоигры");
+        hobbies.add("чтение");
+        String repoUrl = "https://github.com/OhShame/ForJava";
+        String phone = "+79999999999"; // У меня публичный репозиторий, так что тут девяточки х)
+        Collection<String> courseExpectations = new ArrayList<>();
+        courseExpectations.add("начать что-то понимать");
+
+        GreetingImp annGasanova = new GreetingImp(firstName, lastName, birthYear, hobbies,
+                repoUrl, phone, courseExpectations);
+    }
+}
